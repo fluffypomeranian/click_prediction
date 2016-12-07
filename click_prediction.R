ea1243de1be6dd527fcd0456e2546f9b4838757f
Tim Siwula <tcsiwula@usfca.edu>
2016-12-06 17:43:24 -0800

    update

--- click_prediction.R
+++ click_prediction.R
@@ -9,10 +9,8 @@
 ##################################################################
 
 ########################### Setup ################################
-#install.packages("knitr")
-#install.packages("markdown")
-library(knitr)
-library(markdown)
+library(knitr)            #install.packages("knitr")
+library(markdown)         #install.packages("markdown")
 library(ISLR)
 library(tree)
 require("RPostgreSQL")    #install.packages("RPostgreSQL")
@@ -21,18 +19,12 @@ require(tree)             #install.packages("tree")
 require(knitr)
 library(knitr)
 library(markdown)
-#install.packages("e1071", dep = TRUE) # cross validation library
 library(e1071)
-require("e1071")
-#install.packages("ROCR", dep = TRUE) # ROC Curves --> ACCURACY library
-library(ROCR)
+require("e1071")         #install.packages("e1071", dep = TRUE)
+library(ROCR)            #install.packages("ROCR", dep = TRUE)
 require("ROCR")
-#transform the .Rmd to a markdown (.md) file.
-knit("click_prediction.R")
-
+knit("click_prediction.R")  #transform the .Rmd to a markdown (.md) file.
 ####################################################################################
-
-
 
 ########################### SET UP DATABASE CONNECTION ############################
 driver <- dbDriver("PostgreSQL")   # loads the PostgreSQL driver
@@ -44,7 +36,6 @@ connection <- dbConnect(driver, dbname = "clickprediction",
 # confirm the tables are accessible
 dbExistsTable(connection, "clicks_train")
 ##################################################################################
-
 
 ########################### QUERY THE DATABASE ##################################
 # 1)
@@ -61,18 +52,13 @@ getPromotedContent="select * from promoted_content limit 10"
 promoted_content = dbGetQuery(connection, getPromotedContent)
 promoted_content
 
-#TODO
-#remove campagin and adversider id
-#advertiser_id
-#campiagn_id
-
 # join click_train and promoted-content with ad_id new table
 # 500k apears to be stable with rstudio.
 join_query = "
 select t.display_id, t.ad_id, t.clicked, d.document_id,
-d.topic_id, d.confidence_level
+d.topic_id, d.confidence_level, p.advertiser_id
 from clicks_train t, promoted_content p, documents_topics d
-where t.ad_id = p.ad_id and p.document_id = d.document_id
+where t.ad_id = p.ad_id and p.document_id = d.document_id 
 limit 500000;"
 merged_table=dbGetQuery(connection, join_query)
 head(merged_table, 3)
@@ -84,7 +70,6 @@ dbWriteTable(connection, "merged_table", merged_table, row.names=FALSE)
 
 # look at the new table
 getMergedTable="select * from merged_table limit 500000"
-
 new_table = dbGetQuery(connection, getMergedTable)
 
 # list the structure of mydata
@@ -99,7 +84,7 @@ hist(new_table$topic_id)
 ####################################################################################
 
 ########################### GET 50/50 CLICKED ######################################
-
+# balanced data set
 ones = new_table[new_table$clicked>0, 50000]
 length(ones)
 length(new_table)
@@ -125,6 +110,12 @@ dim(final_dataset)
 head(final_dataset, n=5)
 ####################################################################################
 
+################   DATA SKEWNESS    ##############
+my_class = final_dataset$clicked
+# skewness
+plot(skewness(my_class))
+###################################################
+
 ########################### CREATE TRAINING AND TEST SET ###########################
 # define partition ratio
 partition_size = floor(0.80 * nrow(final_dataset)) ## 80% of the sample size
@@ -148,7 +139,7 @@ dim(local_train_set)
 head(local_train_set, n=5)
 #########################################################################
 
-################   FIT REGRESSION TREE TO TRAINING SET     ##############
+################                 DECISION TREE             ##############
 # fit on clicked all features excluding clicked using the final dataset
 clicked_tree = tree(clicked~.-clicked,final_dataset)
 summary(clicked_tree)
@@ -181,9 +172,30 @@ summary(yhat)
 ################   LOGISTIC REGRESSION    ##############
 summary(final_dataset)
 cor(final_dataset)
+partition_size = floor(0.80 * nrow(final_dataset)) ## 80% of the sample size
+
+# remove empty values
+is.na(final_dataset) # if TRUE, then replace them with 0
+final_dataset[is.na(final_dataset)] <- 0 # Not sure replacing NA with 0 will have effect on your model
+#train=sample(1:nrow(final_dataset),partition_size)
+
+partition_size <- floor(0.75 * nrow(final_dataset))
+set.seed(123)
+train_ind <- sample(seq_len(nrow(final_dataset)), size = partition_size)
+train <- final_dataset[train_ind, ]
+test <- final_dataset[-train_ind, ]
 
 # regression
-log_reg_fit=glm(clicked~ . -clicked,data=clicks_train,family=binomial)
+#log_reg_fit=glm(clicked~ . -clicked,data=final_dataset, subset = train,family=binomial)
+
+log_reg_fit=glm(clicked ~ topic_id+confidence_level, data=train,family=binomial)
+summary(log_reg_fit)
+
+glm.probs=predict(log_reg_fit,newdata=test,type="response") 
+glm.pred=ifelse(glm.probs >0.5,1,0)
+#accuracy numberic
+table(glm.pred,test$clicked)
+mean(glm.pred==test$clicked)
 
 #accuracy visuals
 par(mfrow=c(2,2))
@@ -194,23 +206,34 @@ summary(log_reg_fit)
 log_reg_fit
 ##################################################
 
-################   DATA SKEWNESS    ##############
-my_class = final_dataset$clicked
-# skewness
-plot(skewness(my_class))
+################   RANDOM FORESTS    ##############
+#train=sample(1:nrow(final_dataset),300)
+#rf=randomForest(clicked~.-clicked,data=final_dataset,subset=train)
+rf=randomForest(clicked ~ topic_id+confidence_level, data=train, ntree=400)
+rf.probs=predict(rf,test)
+rf.pred=ifelse(rf.probs >0.5,1,0)
 
+#test.err=with(test[-train,],mean((medv-pred)^2))
+
+
+
+# this models accuracy
+table(rf.pred, test$clicked)
+mean(rf.pred == test$clicked)
 ###################################################
 
 
-################   SUPPORT VECTOR MACHINE    ######
-# cross validation library
-# tune() performs 10-fold CV
+# todo
+# create another model with different features
+#m1 : glm with topic_id, conf
+#m2: rf with topic_id, conf
+#m3: glm with topic_id, conf, category_id, category_conf  
+#m4: topics_id+confidence_level+advertiser_id
 
+#more features: entity_id, entity_conf
 
-####### ROC Curves --> ACCURACY ########
-
-
-#####################################################
+#models:       #x-axis: m1, m2
+#accuracy:    #y-axis: 0.60, 0.69
 
 
 ####### SAMPLE SUBMISSION ########
